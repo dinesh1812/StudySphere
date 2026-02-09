b93d63d78da5f7a417040718b0441c493c0aca4b
dinesh1812 <dinesh4119q@gmail.com>
2026-02-09 16:12:56 +0530

    Initial Backend Setup

---
+++ backend/post-service/src/test/java/com/studysphere/post_service/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studysphere.post_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
